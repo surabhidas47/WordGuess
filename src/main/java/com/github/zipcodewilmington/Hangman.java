c81d25371776209b35005c05aea0019d4da07850
surabhi <surabhi.das@outlook.com>
2023-07-12 14:36:46 -0400

    starting with a string Array List

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -1,10 +1,14 @@
 package com.github.zipcodewilmington;
 
 
+import java.util.Random;
+
 /**
  * @author xt0fer
  * @version 1.0.0
  * @date 5/27/21 11:02 AM
  */
 public class Hangman {
+    Random random =new Random();
+
 }
---
+++ src/main/java/com/github/zipcodewilmington/Main.java
@@ -0,0 +1,16 @@
+package com.github.zipcodewilmington;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[]args) {
+
+        ArrayList<String> words = new ArrayList<>();
+        words.add("Giraffe");
+        words.add("Peach");
+        words.add("Cheese");
+        words.add("Vice");
+
+
+    }
+}
