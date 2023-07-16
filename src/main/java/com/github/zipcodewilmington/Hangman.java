f80de7ea183c5ec5c9d579e00730adcc08fdaac1
surabhi <surabhi.das@outlook.com>
2023-07-16 17:50:05 -0400

    random words work

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -1,40 +1,48 @@
-package com.github.zipcodewilmington;
-import java.util.ArrayList;
-import java.util.Random;
-
-/**
- * @author xt0fer
- * @version 1.0.0
- * @date 5/27/21 11:02 AM
- */
-public class Hangman {
-
-    int element =0;
-    private Random random;
-    private ArrayList<String> words;
-
-
-    public Hangman () {
-        this.random = new Random();
-        this.words= new ArrayList<>();
-
-        words.add("Giraffe");
-        words.add("Peach");
-        words.add("Cheese");
-        words.add("Vice");
-        words.add("Heist");
-        //want call a method that picks a random word
-        wordPicked();
-
-    }
-
-    void wordPicked(){
-        //words.size refers to number of elements in the ArrayList...
-        //if 3 elements, words.size = 3
-        //however elements start counting at index 0 per usual
-        int element = random.nextInt(words.size());
-        String pickedWord = words.get(element);
-        System.out.println(pickedWord);
-    }
-
-}
+//package com.github.zipcodewilmington;
+//import java.util.ArrayList;
+//import java.util.Random;
+//
+///**
+// * @author xt0fer
+// * @version 1.0.0
+// * @date 5/27/21 11:02 AM
+// */
+//public class Hangman {
+//
+//    int element =0;
+//    public Random random;
+//    public ArrayList<String> words;
+//
+//
+//
+//
+//    public Hangman () {
+//        this.random = new Random();
+//        this.words= new ArrayList<>();
+//
+//        words.add("Giraffe");
+//        words.add("Peach");
+//        words.add("Cheese");
+//        words.add("Vice");
+//        words.add("Heist");
+//
+//
+//        String ranGen = ranWord();
+//
+//        //checked that words are picked randomly
+//       // System.out.print(ranGen);
+//
+//        //want call a method that picks a random word
+//        ranWord();
+//    }
+//
+//    String ranWord(){
+//        //words.size refers to number of elements in the ArrayList...
+//        //if 3 elements, words.size = 3
+//        //however elements start counting at index 0 per usual
+//        int element = random.nextInt(words.size());
+//        String pickedWord = words.get(element);
+//        return pickedWord;
+//    }
+//
+//}
--- src/main/java/com/github/zipcodewilmington/Main.java
+++ src/main/java/com/github/zipcodewilmington/Main.java
@@ -1,13 +1,85 @@
 package com.github.zipcodewilmington;
 
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[]args) {
+    public static void main(String[] args) {
 
-        Hangman hm = new Hangman();
 
-       // System.out.println(hm);
 
-    }
-}
+        //System.out.println("Enter a single letter:  ");
+        // String userInput =scanner.next();
+
+        String[] wordList = {"giraffe", "cheese", "peach", "vice", "heist"};
+        Random random = new Random();
+        int randomIndex = random.nextInt(wordList.length);
+
+        String randomWord = wordList[randomIndex];
+        int life = randomWord.length();
+        guess(randomWord,life);
+
+
+        System.out.println(randomWord);
+
+
+
+           }
+
+           static public void guess(String randomWord, int life) {
+
+            char [] filler = new char[randomWord.length()];
+            int i=0;
+            while(i< randomWord.length()) {
+                filler[i] = '_';
+                if (randomWord.charAt(i)==' ') {
+                    filler[i]= ' ';
+                }
+                i++;
+            }
+            System.out.println(filler);
+            System.out.println("Life remaining= "+ life);
+
+            Scanner scanner = new Scanner(System.in);
+            ArrayList<Character> dupe= new ArrayList<Character>();
+
+            while(life>0) {
+                char letter = scanner.next().charAt(0);
+
+                if(dupe.contains(letter+"")){
+                    System.out.println("Already entered letter!");
+                    continue;
+                }
+                dupe.add(letter);
+
+                if(randomWord.contains(letter+"")){
+                    for(int j=0;j<randomWord.length();j++){
+                        if(randomWord.charAt(j)==letter){
+                            filler[j]=letter;
+                        }
+                    }
+                }
+                else {
+                   life--;
+                }
+
+                if(randomWord.equals(String.valueOf(filler))){
+                    System.out.println(filler);
+                    System.out.println("You win!!");
+                    break;
+                }
+                System.out.println(filler);
+                System.out.println("Life remaining= "+ life);
+            }
+            if(life==0){
+                System.out.println("You lose :(");
+            }
+           }
+
+
+       }
+
+
+
+
